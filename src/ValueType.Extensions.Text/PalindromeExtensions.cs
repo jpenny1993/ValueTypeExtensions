2500fbced3560e073fa010198a6f7a5fefcbae04
John Penny <jpenny1993@gmail.com>
2018-11-03 14:39:40 +0000

    palindromes, common interview question

---
+++ src/ValueType.Extensions.Text/PalindromeExtensions.cs
@@ -0,0 +1,30 @@
+﻿namespace ValueType.Extensions.Text
+{
+    public static class PalindromeExtensions
+    {
+        /// <summary>
+        /// Checks if string is the same both forwards & reverse.
+        /// </summary>
+        /// <param name="input">The source string</param>
+        /// <returns>True if palindrome</returns>
+        public static bool IsPalindrome(this string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return false;
+            }
+
+            var range = input.Length / 2;
+            var lastIndex = input.Length - 1;
+            for (var index = 0; index < range; index++)
+            {
+                if (input[index] != input[lastIndex - index])
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
