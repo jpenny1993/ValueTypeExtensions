f7f14e8019a04d4edc59701ffe4036478fc2cce7
John Penny <jpenny1993@gmail.com>
2018-11-03 15:19:41 +0000

    anagrams, common interview question

---
+++ src/ValueType.Extensions.Text/AnagramExtensions.cs
@@ -0,0 +1,37 @@
+﻿namespace ValueType.Extensions.Text
+{
+    using System;
+    using System.Linq;
+
+    public static class AnagramExtensions
+    {
+        /// <summary>
+        /// Compares two strings to see if one is an anagram of the other.
+        /// </summary>
+        /// <param name="strA">input string</param>
+        /// <param name="strB">comparison string</param>
+        /// <param name="ignoreCase">ignore case</param>
+        /// <returns>True if is an anagram</returns>
+        public static bool IsAnagramOf(this string strA, string strB, bool ignoreCase = false)
+        {
+            if (string.IsNullOrEmpty(strA) ||
+                string.IsNullOrEmpty(strB) ||
+                strA.Length != strB.Length)
+            {
+                return false;
+            }
+
+            Func<string, string> orderString = (string str) => 
+            {
+                var casedStr = ignoreCase ? str.ToUpperInvariant(): str;
+                var orderedChars = casedStr.OrderBy(c => c);
+                return string.Join(string.Empty, orderedChars);
+            }; 
+
+            var orderedStrA = orderString(strA);
+            var orderedStrB = orderString(strB);
+
+            return string.Equals(orderedStrA, orderString(strB));
+        }
+    }
+}
