0f24befcc4e85aed7863008948cd117ba6188876
John Penny <jpenny1993@gmail.com>
2018-11-03 14:14:56 +0000

    expression expansion

---
+++ src/ValueType.Extensions.Expressions/ExpressionExpansionExtensions.cs
@@ -0,0 +1,28 @@
+﻿namespace ValueType.Extensions.Expressions
+{
+    using System;
+    using System.Linq.Expressions;
+
+    public static class ExpressionExpansionExtensions
+    {
+        public static Expression<TFunc> Expand<TFunc>(this Expression<TFunc> expr)
+        {
+            return new ReduceFinder().Visit(expr) as Expression<TFunc>;
+        }
+
+        private class ReduceFinder : ExpressionVisitor
+        {
+            public override Expression Visit(Expression node)
+            {
+                if (node != null && node.CanReduce)
+                {
+                    var reduced = node.Reduce();
+                    Console.WriteLine("Found expression to reduce!");
+                    Console.WriteLine("Before: {0}: {1}", node.GetType().Name, node);
+                    Console.WriteLine("After: {0}: {1}", reduced.GetType().Name, reduced);
+                }
+                return base.Visit(node);
+            }
+        }
+    }
+}
